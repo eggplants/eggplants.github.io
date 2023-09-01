16c058e21122971205823057a47304d1b50efad3
eggplants <w10776e8w@yahoo.co.jp>
2023-09-01 20:28:13 +0900

    seitai

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
 (function () {
   "use strict";
-  console.log('%c Hi!', 'font-size:180px;background:url(https://raw.githubusercontent.com/eggplants/myicons/master/selfie_mini.png)no-repeat;');
-  var t = document.title.split(/(?<=)/u);
+  let t = document.title.split(/(?<=)/u);
   Array.prototype.rotate = function (n) {
     n %= this.length;
     return this.slice(n, this.length).concat(this.slice(0, n));
   };
-  var cnt = 0;
+  let cnt = 0;
   setInterval(() => {
     document.title = (t = t.rotate(2)).join("");
   }, 500);
-  var i = setInterval(() => {
+  let i = setInterval(() => {
     if (cnt == 3) {
       window.location.href = "https://github.com/eggplants";
       clearInterval(i);
